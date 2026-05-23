4a795f24608808d9e44d56ebc5fa8941b3a0c40e
PandesalPanpan <petermarticio@gmail.com>
2026-05-23 04:48:35 +0000

    chore: default dev server to port 5174 to avoid clash with another project

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       },
     }),
   ],
-  server: { host: true, port: 5173, watch: { usePolling: true } },
+  server: { host: true, port: 5174, watch: { usePolling: true } },
   preview: { host: true, port: 4173 },
   test: {
     globals: true,
